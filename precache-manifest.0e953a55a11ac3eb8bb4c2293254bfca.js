1a7a9e95a2a1cee6e28c78ba5a231d9ad7fba95c
rahbaran <amirrezarahbaran@gmail.com>
2019-01-28 00:11:25 +0330

    Updates

---
+++ precache-manifest.0e953a55a11ac3eb8bb4c2293254bfca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/todo/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "8a695b30138abc35248a",
+    "url": "/todo/static/js/runtime~main.8a695b30.js"
+  },
+  {
+    "revision": "85238872f99ce7ca4a9a",
+    "url": "/todo/static/js/main.85238872.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/todo/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "85238872f99ce7ca4a9a",
+    "url": "/todo/static/css/main.f7f67932.chunk.css"
+  },
+  {
+    "revision": "ed310f671bb3d58f91454237555a911b",
+    "url": "/todo/index.html"
+  }
+];
