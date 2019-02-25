1d8309925439df9567c162ca0c9121e7495afd89
AmirrezaRahbaran <amirrezarahbaran@gmail.com>
2019-02-25 16:16:33 +0330

    the initial todo css is applied

--- config-overrides.js
+++ config-overrides.js
@@ -1,5 +1,7 @@
+const rewireMobX = require('react-app-rewire-mobx');
+ 
+/* config-overrides.js */
 module.exports = function override(config, env) {
-  //do stuff with the webpack config...
+  config = rewireMobX(config, env);
   return config;
 }
-
--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+// import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+      <div id="todoapp" className="todoapp">
+        <header className="header">
+          <h1>todo</h1>
+          <input
+            type="text"
+            className="new-todo"
+            placeholder="What needs to be done?"
+          />
         </header>
+        <section className="main">
+          <ul className="todo-list">
+            <li>
+              <div className="view">
+                <input type="checkbox" className="toggle" value="on"/>
+                <label>new work</label>
+                <button className="destory" />
+              </div>
+            </li>
+            <li className="completed">
+              <div className="view">
+                <input type="checkbox" className="toggle" value="on" checked={true}/>
+                <label>old work</label>
+                <button className="destory" />
+              </div>
+            </li>
+          </ul>
+        </section>
       </div>
     );
   }
